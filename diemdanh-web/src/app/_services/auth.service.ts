d5c2fde1b832338010a13df3cfacece4d68faeec
Jacking Tai <kenvintai1707@gmail.com>
2020-02-04 21:03:21 +0700

    update QLGV
    layout, function,...

--- diemdanh-web/src/app/_services/auth.service.ts
+++ diemdanh-web/src/app/_services/auth.service.ts
@@ -1,18 +1,77 @@
 import { Injectable } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
-import {  map} from 'rxjs/operators';
+import { AngularFireDatabase, AngularFireList, AngularFireObject } from '@angular/fire/database';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   UsersRef: AngularFireList < any > = null;
+  GVRef: AngularFireList < any > = null;
+  ClassRef: AngularFireObject<any>;
+  GV;
+  Class;
   currentUser:any;
   returnUrl: string;
   constructor(private router: Router,private db: AngularFireDatabase, private route: ActivatedRoute) {
     // get url query
     this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+
+    this.getGV();
+    this.getClass();
+   }
+   getGV(){
+    this.GVRef = this.db.list('GV');
+    this.GVRef.snapshotChanges()
+    .pipe(map(items => { // <== new way of chaining
+        return items.map(a => {
+            const data = a.payload.val();
+            const key = a.payload.key;
+            return {
+                key, ...data
+            }; // or {key, ...data} in case data is Obj
+        });
+    })).subscribe(gv =>{
+      this.GV= gv;
+    });
+  }
+  getClass(){
+    // this.ClassRef= this.db.object('LOP');
+    // this.ClassRef.snapshotChanges().pipe(map(item =>{
+    //   const data = item.payload.val();
+    //   const key = item.key;
+    //   // return {
+    //   //   key, ...data
+    //   // };
+    //   return data
+    // })).subscribe(data=>{
+    //   this.Class= data;
+    //   // this.Class=Object.keys(data).map(key => data[key]);
+    //   console.log("covert"+Object.keys(data).map(key => data[key]))
+    //   console.log("data: "+data);
+    // })
+    this.db.list('LOP').valueChanges().subscribe(lop => {
+      this.Class= lop;
+      console.log(this.Class);
+    });
+  }
+   addGV(newdata){
+    this.GVRef = this.db.list('GV');
+    this.GVRef.push(newdata);
+    console.log('added');
+   }
+   updateGV(key, data){
+    this.db.list('GV').update(key, data);
+    console.log("updated");
+   }
+   delete(data){
+     this.db.list('GV').remove(data);
+     console.log("removed")
+   }
+   updateClass(key, data){
+    this.db.list('LOP').update(key, data);
    }
   sendToken(email, password) {
     this.UsersRef = this.db.list('root');
--- diemdanh-web/src/app/quanly-gv/quanly-gv.component.ts
+++ diemdanh-web/src/app/quanly-gv/quanly-gv.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-quanly-gv',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quanly-gv.component.css']
 })
 export class QuanlyGVComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  userLoggedIn;
+  GiaoVien;
+  edit= false;
+  info_edit: any;
+  newteacher={
+    "email": "",
+    "password": "",
+    "msgv": "",
+    "tengv": "",
+    "ngaysinh": "",
+    "lop": ""
+  };
+  Class;
+  constructor(private auth: AuthService) {
+    this.userLoggedIn =JSON.parse(this.auth.getToken());
   }
 
+  ngOnInit() {
+    this.GiaoVien=this.auth.GV;
+  }
+  addGV(){
+    this.auth.addGV(this.newteacher);
+  }
+  editGV(data){
+    this.info_edit= data;
+    this.edit= true;
+    console.log(this.info_edit)
+  }
+  delete(data){
+    this.auth.delete(data.key)
+  }
+  updateGV(){
+    this.auth.updateGV(this.info_edit.key, this.info_edit);
+    let keylop=this.auth.Class.find(s => s.tenlop=== this.info_edit.lop)
+    this.auth.updateClass(keylop.key, {"tengv": this.info_edit.tengv});
+  }
 }
