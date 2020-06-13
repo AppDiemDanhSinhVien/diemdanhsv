b1b298bcee34e7d4bd46182e60e27b6bbc5e6d87
Jacking Tài <54407688+jackingit@users.noreply.github.com>
2020-06-13 23:27:02 +0700

    update

--- diemdanh-web/src/app/_services/auth.service.ts
+++ diemdanh-web/src/app/_services/auth.service.ts
@@ -10,34 +10,38 @@ export class AuthService {
   UsersRef: AngularFireList < any > = null;
   GVRef: AngularFireList < any > = null;
   ClassRef: AngularFireObject<any>;
+  MonRef: AngularFireObject<any> = null;
   StudentRef: AngularFireList<any>
   GV; // <= tất cả giáo viên  
   Class; // <= tất cả các lớp
   YClass; // <= lớp chưa có giáo viên
   Student; // <= tất cả học sinh
+  MonHoc; // <= tất cả môn
   currentUser:any;
   returnUrl: string;
   constructor(private router: Router,private db: AngularFireDatabase, private route: ActivatedRoute) {
     // get url query
     this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
-
     this.getGV();
+    this.getMon();
     this.getClass();
     this.getStudent();
-   }
-   getGV(){
+  }
+  getGV(){
     this.GVRef = this.db.list('GV');
     this.GVRef.snapshotChanges()
     .pipe(map(items => { // <== new way of chaining
         return items.map(a => {
             const data = a.payload.val();
-            const key = a.payload.key;
+            const id = a.payload.key;
             return {
-                key, ...data
+                id, ...data
             }; // or {key, ...data} in case data is Obj
         });
     })).subscribe(gv =>{
       this.GV= gv;
+      console.log("All teacher: ")
+      console.log(gv);
     });
   }
   getClass(){
@@ -70,47 +74,79 @@ export class AuthService {
         });
     })).subscribe(cc =>{
       this.Student= cc
-      console.log(this.Student)
+      // console.log(this.Student)
   })}
+  // get all Mon
+  getMon(){
+    let MonRef: AngularFireList < any > =  this.db.list('MonHoc');
+    MonRef.snapshotChanges()
+    .pipe(map(items => { // <== new way of chaining
+        return items.map(a => {
+            const data = a.payload.val();
+            const id = a.payload.key;
+            return {
+                id, ...data
+            }; // or {key, ...data} in case data is Obj
+        });
+    })).subscribe(mon => {
+      this.MonHoc= mon;
+      console.log("All MonHoc: ");
+      console.log(mon);
+    });
+  }
   // Class no have teacher + this class
   ClassNoHaveTeacher(){
-    let result=[];
-    this.Class.forEach(e => {
-      if(e.tengv == null || e.tengv == ''){
-        result.push(e);
+    let result=this.MonHoc.filter(m => m.tengv == null || m.tengv == '');
+    let rt= result.map(data => {
+      return {
+        id : data.id,
+        lichday: data.lichday,
+        tenmonhoc: data.tenmonhoc
       }
-    })
-    // console.log(result);
-    return result;
+    });
+    return rt;
   }
   // Class have teacher
   ClassHaveTeacher(){
-    let result=[];
-    this.Class.forEach(e => {
-      if(e.tengv != null){
-        result.push(e);
+    let result= this.MonHoc.filter(m => m.tengv != null);
+    let rt= result.map(data => {
+      return {
+        id : data.id,
+        lichday: data.lichday,
+        tenmonhoc: data.tenmonhoc
       }
-    })
-    // console.log(result);
-    return result;
+    });
+    return rt;
   }
-   addGV(newdata){
+  // Add new teacher
+  async addGV(newdata){
     this.GVRef = this.db.list('GV');
-    this.GVRef.push(newdata);
-    console.log('added');
-    this.getGV();
-   }
-   updateGV(key, data){
-    this.db.list('GV').update(key, data);
-    console.log("updated");
-   }
-   delete(data){
-     this.db.list('GV').remove(data);
-     console.log("removed")
-   }
-   updateClass(key, data){
-    this.db.list('LOP').update(key, data);
-   }
+    await this.GVRef.push(newdata);
+    await console.log('added');
+    await this.getGV();
+    await this.ClassNoHaveTeacher();
+  }
+
+  // Update info teacher
+  async updateGV(key, data){
+    await this.db.list('GV').update(key, data);
+    await console.log("updated");
+    await this.ClassNoHaveTeacher();
+  }
+
+  // Delete data teacher
+  async delete(id){
+    await this.db.list('GV').remove(id);
+    await console.log("removed");
+    await this.ClassNoHaveTeacher();
+  }
+
+  // Update info in tab MonHoc
+  async updateMon(key, data){
+    await this.db.list('MonHoc').update(key, data);
+    await console.log("updated MonHoc")
+    await this.ClassNoHaveTeacher();
+  }
   sendToken(email, password) {
     this.UsersRef = this.db.list('root');
     this.UsersRef.snapshotChanges()
--- diemdanh-web/src/app/quanly-gv/quanly-gv.component.ts
+++ diemdanh-web/src/app/quanly-gv/quanly-gv.component.ts
@@ -11,113 +11,153 @@ export class QuanlyGVComponent implements OnInit {
   GiaoVien; // <= danh sách giáo viên
   edit= false; // <= trạng thái edit
   edit_on; // <= data khi chỉnh sửa
+  modal_stt= true; // <= trạng thái modal (Add new GV)
+  monhoc; // <== tất cả môn học
+  noTeacher: []; // <= lớp không có GV
   newteacher={
     "email": "",
     "password": "",
     "msgv": "",
     "tengv": "",
     "ngaysinh": "",
-    "lop": []
+    "mon": []
   }; // <= data khi thêm giáo viên
-  classes; // <= data giáo viên chưa có lớp dạy
-  class_edit; // <= lớp đang chính sửa
+  class_edit; // <= lớp đang chỉnh sửa
   constructor(private auth: AuthService) {
     this.userLoggedIn =JSON.parse(this.auth.getToken());
     this.GiaoVien=this.auth.GV;
+    // this.monhoc= this.auth.MonHoc;
   }
 
   ngOnInit() {
-
-  }
-  // function when add new teacher (don't used)
-  async addnew(){
-    let cls= this.auth.Class;
-    let result=[];
-    await cls.forEach(e => {
-      if(e.tengv == null || e.tengv == ''){
-        result.push(e);
-      }
-    })
-    this.classes= result;
-    console.log(this.classes);
+    this.noTeacher= this.auth.ClassNoHaveTeacher();
+    this.monhoc= this.auth.MonHoc;
   }
 
-  // function check class of no have teacher (don't used)
-  checkClass(item){
-    if(this.auth.YClass.find(a => a.key === item.key)){
-      return true;
-    }else{
-      return false;
-    }
-  }
   // function when edit class of GV when add new GV
   onChange(checked, item){
     if(checked){
-    this.newteacher.lop.push(item);
-    } else {
-      this.newteacher.lop.splice(this.newteacher.lop.indexOf(item), 1)
+      this.newteacher.mon.push(item);
+      console.log(this.newteacher.mon);
+    }else{
+      this.newteacher.mon.splice(this.newteacher.mon.indexOf(item) , 1)
+      console.log(this.newteacher.mon);
     }
-    // console.log(this.newteacher.lop)
   }
 
   // function when edit class of GV when edit GV
   onChangeEdit(checked, item){
     if(checked){
-      this.edit_on.lop.push(item);
+      this.edit_on.mon.push(item);
+      console.log(this.edit_on.mon);
     } else {
-      this.edit_on.lop.splice(this.edit_on.lop.indexOf(item), 1)
+      this.edit_on.mon.splice(this.edit_on.mon.indexOf(item), 1);
+      console.log(this.edit_on.mon);
     }
-    console.log("Danh sách lớp có thể chọn: ");
-    console.log(this.ClassNoHaveTeacher());
   }
+
   // function when click button add new GV
   async addGV(){
+    // update tab MonHoc
+    await this.newteacher.mon.forEach(e => {
+      this.auth.updateMon(e.id, {"tengv" : this.newteacher.tengv})
+    });
+
+    // update tab GV
     await this.auth.addGV(this.newteacher);
-    this.newteacher={
-      "email": "",
-      "password": "",
-      "msgv": "",
-      "tengv": "",
-      "ngaysinh": "",
-      "lop": []
+
+    // hidden modal (add new GV)
+    await this.hiddenModal('close_add_modal');
+
+    // set info = null
+    this.newteacher= {
+      "email": null,
+      "password": null,
+      "msgv": null,
+      "tengv": null,
+      "ngaysinh": null,
+      "mon": []
     };
-    await this.ClassHaveTeacher();
-    await this.ClassNoHaveTeacher();
+    
+    this.noTeacher= await this.auth.ClassNoHaveTeacher();
   }
-  // function when click edit GV
-   editGV(data){
-    this.edit_on= data;
-    this.edit= true;
-    var k= this.auth.ClassNoHaveTeacher();
-    this.class_edit=this.edit_on.lop.concat(k);
-  }
-  // check disable when is class have teacher
-  isDisabled(item){
-    if(this.ClassNoHaveTeacher().indexOf(item) == -1){
-      return true;
-    }else{
-      return false;
-    }
-  }
-  // checked when teacher teaching this class
-  isChecked(keys){
-    let x= false;
-    if(this.edit_on.lop != null){
-      for(let i=0; i< this.edit_on.lop.length; i++){
-        if(this.edit_on.lop[i].key === keys){
-          x= true;
-          break;
-        }else{
-          x=false;
-          break;
-        }
+
+  // function update info GV
+  async updateGV(){
+    // update tab GV
+    await this.auth.updateGV(this.edit_on.id, this.edit_on);
+
+    // get all id of monhoc in tab GV
+    await this.auth.getGV();
+    let tabGV=[];
+    await this.auth.GV.forEach(gv => {
+      gv.mon.forEach(mon => {
+        tabGV.push(mon.id)
+      });
+    });
+    console.log("all id of monhoc in tab GV: ");
+    console.log(tabGV);
+
+    // update name of teacher in tab MonHoC
+    if(this.edit_on.mon != null){
+      for(let i=0; i< this.edit_on.mon.length; i++){
+        this.auth.updateMon(this.edit_on.mon[i].id, {"tengv": this.edit_on.tengv});
       }
-      return x;
+    }
+
+    // get all id of monhoc in tab MonHoc
+    let tabMonHoc=[];
+    for(let i=0; i< this.auth.MonHoc.length; i++){
+      await tabMonHoc.push(this.auth.MonHoc[i].id);
+    }
+    await console.log("all id of monhoc in tab LOP: ");
+    await console.log(tabMonHoc);
+
+    // filter id of monhoc no have teacher
+    let result= tabMonHoc;
+    await tabGV.forEach(e => {
+      if(tabMonHoc.indexOf(e) > -1){
+        result.splice(tabMonHoc.indexOf(e), 1);
+      }
+    })
+    await console.log("all key of class no have teacher: ");
+    await console.log(result);
+
+    // update tab MonHoc when have non teacher in class
+    await result.forEach(p => {
+      this.auth.updateMon(p, {tengv: ""})
+    });
+
+    this.noTeacher= await this.auth.ClassNoHaveTeacher();
+    
+    // hidden modal (add new GV)
+    await this.hiddenModal('close_edit_modal');
+  }
+
+  // function when click edit GV
+  async editGV(data){
+    console.log(data);
+    this.edit= await true; // bật trạng thái đang chỉnh sửa
+    this.edit_on= await data; // đổ dữ liệu
+    if(this.edit_on.mon){
+      this.class_edit= await this.edit_on.mon;
+    }else{
+      this.edit_on.mon= await [];
+      this.class_edit= await [];
+    }
+    let k= await this.auth.ClassNoHaveTeacher(); //list những lớp chưa có giáo viên
+    if(this.class_edit.length == 0){
+      this.class_edit= await this.auth.ClassNoHaveTeacher();
+      await console.log(this.class_edit);
+    }else{
+      this.class_edit= await this.class_edit.concat(k);
+      await console.log(this.class_edit);
     }
   }
-  
+
   // Class no have teacher + this class
   ClassNoHaveTeacher(){
+    this.noTeacher= this.auth.ClassNoHaveTeacher();
     return this.auth.ClassNoHaveTeacher();
   }
 
@@ -126,52 +166,24 @@ export class QuanlyGVComponent implements OnInit {
     return this.auth.ClassHaveTeacher();
   }
 
-  delete(data){
-    this.auth.delete(data.key)
-  }
-  async updateGV(){
-    if(this.edit_on.lop != null){
-      // update name of teacher in tab LOP
-      for(let i=0; i< this.edit_on.lop.length; i++){
-        this.auth.updateClass(this.edit_on.lop[i].key, {"tengv": this.edit_on.tengv});
-      }
-    }else{
-    }
-
-    // update tab GV
-    await this.auth.updateGV(this.edit_on.key, this.edit_on);
-
-    // get all key of class in tab LOP
-    let lop=[];
-    for(let i=0; i< this.auth.Class.length; i++){
-      await lop.push(this.auth.Class[i].key);
-    }
-    console.log("all key of class in tab LOP: "+ lop);
-
-    // get all key of class in tab GV
-    this.auth.getGV();
-    let cls=[];
-    await this.auth.GV.forEach(e => {
-      e.lop.forEach(h => {
-        cls.push(h.key)
-      });
+  // Remote GV 
+  async delete(data){
+    await data.mon.forEach(e => {
+      this.auth.updateMon(e.id, {tengv: ""})
     });
-    console.log("all key of class in tab GV: "+cls);
+    await console.log(data.id);
+    await this.auth.delete(data.id);
+    this.noTeacher= await this.auth.ClassNoHaveTeacher();
+  }
 
-    // filter key of class non have teacher
-    await cls.forEach(e => {
-      if(lop.indexOf(e) > -1){
-        lop.splice(lop.indexOf(e), 1);
-      }
-    })
-    console.log("all key of class no have teacher: "+ lop);
-
-    // update tab LOP when have non teacher in class
-    for(let i=0; i< lop.length; i++){
-      await this.auth.updateClass(lop[i], {tengv: null});
-    }
-
-    await this.ClassNoHaveTeacher();
-    await this.ClassHaveTeacher();
+  // function hidden modal (enter id of button close);
+  hiddenModal(id){
+    let elem = document.getElementById(id);
+    let evt = new MouseEvent('click', {
+            bubbles: true,
+            cancelable: true,
+            view: window
+        });
+    elem.dispatchEvent(evt);
   }
 }
